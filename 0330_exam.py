504b603ad578eecef4c77ace3403938a938cedef
DESKTOP-2U7ISNC\pcn04 <kissflower46@gmail.com>
2022-03-30 17:59:25 +0900

    파이썬 문제푸는 중

---
+++ 0330_exam.py
@@ -0,0 +1,41 @@
+# 1
+# from posixpath import split
+
+
+# def chg(t):
+#     chgt = t.split(':')
+
+#     result = "#".join(chgt)
+#     print(result)
+
+
+# text = "a:b:c:d"
+
+# chg(text)
+
+# 2
+# a = {'A': 90, 'B': 80}
+
+# try:
+#     print(a['C'])
+# except:
+#     print("70")
+
+# 3
+# a = [1, 2, 3]
+
+# print(id(a))
+
+# #a = a + [4, 5]
+
+# a.extend([4, 5])
+
+# print(id(a))
+# +를 하고나면 주소값이 바뀌지만 extend를 하게 되면 주소값이 그대로 유지된다.
+
+# 4
+# A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
+
+# for i in A:
+#     if i >= 50:
+#         print(i)
