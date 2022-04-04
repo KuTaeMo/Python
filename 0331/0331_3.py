2a98af3ad1280adf483b2dd865b0b6dce3424194
DESKTOP-2U7ISNC\pcn04 <kissflower46@gmail.com>
2022-04-04 14:50:49 +0900

    정리

--- 0331/0331_3.py
+++ 0331/0331_3.py
@@ -1,4 +1,5 @@
 import re
+from string import whitespace
 
 
 def refinder(pattern, script):
@@ -17,3 +18,13 @@ def refinder_all(pattern, script):
 
 refinder(r'is', 'life is so cool')
 refinder_all(r'is', 'life is so cool')
+
+# 정규표현식
+
+# \d 숫자와 매치, [0-9]와 같음
+# \D 숫자가 아닌 것과 매치, ^[0-9]와 같음
+# \s whitespace 문자와 매치, [\t\n\r\f\v]와 같음
+# \S whitespace 문자가 아닌 것과 매치, [^\t\n\r\f\v]와 같음
+# \w 문자 + 숫자와 문자와 매치, [a-zA-Z0-9_]와 같음
+# \W 문자 + 숫자와 문자가 아닌 문자와 매치, [^a-zA-Z0-9_]와 같음
+# \\ 메타 문자가 아닌 일반 문자 역슬래시(\)와 매치, 메타 문자 앞에 \를 붙이면 일반 문자를 의미
